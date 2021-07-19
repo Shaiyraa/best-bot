f73d301c4f3937cf399a957f84e78b51524bbb04
Karina Kuss <karina.kuss00@gmail.com>
2021-07-19 22:41:39 +0200

    created a command for showing guild config

--- commands/config/showConfig.js
+++ commands/config/showConfig.js
@@ -1,4 +1,22 @@
-module.exports = (message) => {
+const Discord = require('discord.js');
+const isGuildInDB = require('../../utils/isGuildInDB');
+const sendEmbedMessage = require('../../utils/sendEmbedMessage');
 
-  message.channel.send("This command is not supported yet.")
+module.exports = async (message) => {
+
+  // 1. CHECK IF CONFIG EXISTS
+  const guildConfig = await isGuildInDB(message);
+  if (!guildConfig) return;
+
+  // 2. CREATE EMBED
+  const embed = new Discord.MessageEmbed()
+  .setTitle("Config:")
+  .addField("Member role:", `<@&${guildConfig.memberRole}>`)
+  .addField("Officer role:", `<@&${guildConfig.officerRole}>`)
+  .addField("Announcements channel:", `<#${guildConfig.announcementsChannel}>`)
+  .addField("Reminders channel:", `<#${guildConfig.remindersChannel}>`)
+  .addField("Default message:", guildConfig.defaultEventMessage);
+  
+  // 3. SEND EMBED
+  message.channel.send(embed);
 };
